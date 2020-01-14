8fad699f8341f390651ec64e1772663ab02b0fe0
Bobcat Jogger <jacob@gotberg.net>
2020-01-14 07:50:51 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+
+const url = 'https://apex.prosperousuniverse.com/login';
+
+puppeteer
+      .launch()
+      .then(browser => browser.newPage())
+      .then(page => {
+        return page.goto(url).then(function() {
+          return page.content();
+        });
+      })
+      .then(html => {
+        const $ = cheerio.load(html);
+        const loginPage = $('.login-page');
+        console.log(loginPage);
+      })
+      .catch(console.error);
