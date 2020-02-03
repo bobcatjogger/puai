30f01bd823b4469f25d8933d0ad893db67bad720
Jake Gotberg <jake@traxia.com>
2020-02-03 07:53:04 -0600

    adding in playwright

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ require('dotenv').config();
 const util = require('./utils');
 const readline = require('readline');
 const moment = require('moment');
+const playwright = require('playwright');
+
+(async () => {
+  for (const browserType of ['chromium', 'firefox', 'webkit']) {
+    const browser = await playwright[browserType].launch();
+    const context = await browser.newContext();
+    const page = await context.newPage('http://whatsmyuseragent.org/');
+
+    await page.screenshot({ path: `example-${browserType}.png` });
+    await browser.close();
+  }
+})();
 
 //   await page.goto(loginUrl, { waitUntil: 'networkidle2' });
 //   const emailInput = await page.waitFor('#login')
